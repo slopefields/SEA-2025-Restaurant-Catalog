f52a8d80ff3a15737cbeec9b334fe34d13d2bdef
slopefields <yooje44@gmail.com>
2025-04-13 10:23:44 -0700

    Create restaurant objects for use in scripts.js

---
+++ restaurant-data.js
@@ -0,0 +1,492 @@
+// URLs to images for each restaurant
+const PAPA_JOHNS_URL = "https://s3-media0.fl.yelpcdn.com/bphoto/0hzy-85PItw-nohevt0Q9g/o.jpg";
+const BOILING_POINT_URL = "https://s3-media0.fl.yelpcdn.com/bphoto/9KdDVxXi7q12xS4Z_NOK7A/o.jpg";
+const MCDONALDS_URL = "https://s3-media0.fl.yelpcdn.com/bphoto/8gW1AUGU7u-xK8FxuDPt-A/o.jpg";
+const OOTORO_URL = "https://s3-media0.fl.yelpcdn.com/bphoto/OQyktvrYDGbIjOHGwAcysA/o.jpg"; 
+const BURGER_KING_URL = "https://s3-media0.fl.yelpcdn.com/bphoto/CossT1WTwgjK7I9fpoYmHA/o.jpg";
+const CAVA_URL = "https://s3-media0.fl.yelpcdn.com/bphoto/kufCz6Jy79lc2q-9F0_6Xg/o.jpg";
+const CHIPOTLE_URL = "https://s3-media0.fl.yelpcdn.com/bphoto/jzOW_cvF4NGMhwvSmW3c_A/o.jpg";
+const POPEYES_URL = "https://s3-media0.fl.yelpcdn.com/bphoto/zOMTAUeGGlKbY-W4Gm4ehg/o.jpg";
+const STARBUCKS_URL = "https://s3-media0.fl.yelpcdn.com/bphoto/pvLAqMMY5nXm7kdeZnwKhQ/o.jpg";
+const CHEESECAKE_FACTORY_URL = "https://s3-media0.fl.yelpcdn.com/bphoto/WhVZpJORImlvnTb2-C4IIg/o.jpg";
+const NOBU_URL = "https://s3-media0.fl.yelpcdn.com/bphoto/mpt7oW0XUj-VR-N900sQSA/o.jpg";
+const GUELAGUETZA_URL = "https://s3-media0.fl.yelpcdn.com/bphoto/M7qeSDej3D5xOVmeaVgXug/o.jpg";
+const IN_N_OUT_URL = "https://s3-media0.fl.yelpcdn.com/bphoto/MGgmplI0ZnEIezUIwxbRJg/348s.jpg";
+const THE_HARD_ROCK_CAFE_URL = "https://s3-media0.fl.yelpcdn.com/bphoto/gGtO4CPrUiMnT6BkL6EKag/348s.jpg";
+const BESTIA_URL = "https://s3-media0.fl.yelpcdn.com/bphoto/MqIBArvXWucYCyN1-7qI3A/o.jpg";
+const URTH_CAFFE_URL = "https://s3-media0.fl.yelpcdn.com/bphoto/3JzZMoAUJjq71YB9m7vy0g/o.jpg";
+const THE_SPOT_URL = "https://s3-media0.fl.yelpcdn.com/bphoto/jpDpoi35v6N53X1e8VUnGA/o.jpg";
+const THE_WALKING_STICK_URL = "https://s3-media0.fl.yelpcdn.com/bphoto/YfI5i1frSyHvGaLz90fyhw/348s.jpg";
+const SHUGA_CAFE_URL = "https://s3-media0.fl.yelpcdn.com/bphoto/VPlRhWr-goB-BJJu3yH4sQ/348s.jpg";
+const THE_STONE_URL = "https://s3-media0.fl.yelpcdn.com/bphoto/21hkTg9I_JI5nLpzvjkuIA/348s.jpg";
+const WINGSTOP_URL = "https://s3-media0.fl.yelpcdn.com/bphoto/R7fGhe345JPGZbeR35Cznw/348s.jpg";
+const PETERS_CAFE_URL = "https://s3-media0.fl.yelpcdn.com/bphoto/J3_WGu7kmoQnd8OSnhIhQA/o.jpg";
+const THE_BEST_FISH_TACO_URL = "https://s3-media0.fl.yelpcdn.com/bphoto/WylMteiK0g18zt4yPfrIFQ/348s.jpg";
+const THE_RIALTO_URL = "https://s3-media0.fl.yelpcdn.com/bphoto/ayUDA_HxGYwXPBxDP5bpwA/o.jpg";
+const THE_BOWERY_URL = "https://s3-media0.fl.yelpcdn.com/bphoto/8_el94D7g4uKVU9MliT2mA/o.jpg";
+const THE_SHED_URL = "https://s3-media0.fl.yelpcdn.com/bphoto/_6NT_rv0qK7YKI3vzDfhTw/o.jpg";
+const SUGARFISH_URL = "https://s3-media0.fl.yelpcdn.com/bphoto/nKM6R2OVheeuXGHc3BWc0w/o.jpg";
+const LAYLA_CAFE_URL = "https://s3-media0.fl.yelpcdn.com/bphoto/nJSHevdkwA325VOGsguP2Q/348s.jpg";
+const GRILL_EM_UP_URL = "https://s3-media0.fl.yelpcdn.com/bphoto/hRk8reS__JGajRAT035I0Q/348s.jpg";
+const FRANKS_FAMOUS_URL = "https://s3-media0.fl.yelpcdn.com/bphoto/AJkwfuhEatZCi-z8QQwPQA/348s.jpg";
+const JUST_FIRE_URL = "https://s3-media0.fl.yelpcdn.com/bphoto/oLqSgLpY1PDSrWCqY9Dmzw/348s.jpg";
+const KABOB_CHAI_URL = "https://s3-media0.fl.yelpcdn.com/bphoto/GxvAmlOLUf1f2kNUc3j0tw/l.jpg";
+const HAPPY_GARDEN_URL = "https://s3-media0.fl.yelpcdn.com/bphoto/3ySQ-nXVhJ_3atEVxI2ApA/348s.jpg";
+const STELLA_URL = "https://s3-media0.fl.yelpcdn.com/bphoto/q1NoN0OeLO30JNTvgbZpYQ/o.jpg";
+const THE_KITCHEN_URL = "https://s3-media0.fl.yelpcdn.com/bphoto/JDo6NVoLDP8WBLWOgBbRQw/o.jpg";
+const THE_RIDGE_URL = "https://s3-media0.fl.yelpcdn.com/bphoto/mR-K69iu-RHawiAmwhQSnA/348s.jpg";
+const BOLOVINO_URL = "https://s3-media0.fl.yelpcdn.com/bphoto/-hZgNHccB-Qa4KABR8TXxA/1000s.jpg";
+const TACOS_AND_TEQUILA_URL = "https://s3-media0.fl.yelpcdn.com/bphoto/SqtojNroTWIkpab6-m-nFw/o.jpg";
+const HORIZON_TAVERN_URL = "https://s3-media0.fl.yelpcdn.com/bphoto/9oz-iisahf3RnufgkHJFQA/348s.jpg";
+const GABRIELA_URL = "https://s3-media0.fl.yelpcdn.com/bphoto/jFX87p8ZhJTAY1CYf6pMXg/o.jpg";
+const AZZURRO_URL = "https://s3-media0.fl.yelpcdn.com/bphoto/9Dv0PwQcCBNHevBYPzl2Pg/348s.jpg";
+const THRILLER_URL = "https://s3-media0.fl.yelpcdn.com/bphoto/9hYZYb-HJqa-wkVkRjDG8g/348s.jpg";
+const GATEWAY_KITCHEN_URL = "https://s3-media0.fl.yelpcdn.com/bphoto/pcfUv7weqNuj75np2Av8HQ/1000s.jpg";
+const LA_BROOKLYN_URL = "https://s3-media0.fl.yelpcdn.com/bphoto/Py7RdBp9qiFBrFif4MG1bw/348s.jpg";
+const CAJUN_QUEEN_URL = "https://s3-media0.fl.yelpcdn.com/bphoto/KXNvXpLanZRWW9qz7wsxbg/o.jpg";
+
+// Objects containing each restaurant's information
+const PAPA_JOHNS = {
+  name: "Papa John's",
+  address: "15435 Jeffrey Rd Ste 114 Irvine, CA 92618",
+  rating: "2.0",
+  reviewCount: 222,
+  categories: "Pizza, Chicken Wings, Fast Food",
+  image: PAPA_JOHNS_URL,
+}
+const BOILING_POINT = {
+  name: "Boiling Point",
+  address: "14140 Culver Dr Ste A Irvine, CA 92604",
+  rating: "3.4",
+  reviewCount: 2447,
+  categories: "Taiwanese, Hot Pot, Comfort Food",
+  image: BOILING_POINT_URL,
+}
+const MCDONALDS = {
+  name: "McDonald's",
+  address: "119 E Chapman Ave Fullerton, CA 92832",
+  rating: "1.8",
+  reviewCount: 185,
+  categories: "Fast Food, Burgers, Coffee & Tea",
+  image: MCDONALDS_URL,
+}
+const OOTORO = {
+  name: "Ootoro Sushi",
+  address: "2222 Michelson Dr Ste 246 Irvine, CA 92612",
+  rating: "4.2",
+  reviewCount: 869,
+  categories: "Sushi Bars, Japanese",
+  image: OOTORO_URL,
+}
+const BURGER_KING = {
+  name: "Burger King",
+  address: "9500 Gilman Dr San Diego, CA 92093",
+  rating: "2.1",
+  reviewCount: 51,
+  categories: "Fast Food, Burgers",
+  image: BURGER_KING_URL,
+}
+const CAVA = {
+  name: "Cava",
+  address: "20700 Avalon Blvd Carson, CA 90746",
+  rating: "3.8",
+  reviewCount: 116,
+  categories: "Mediterranean, Salad",
+  image: CAVA_URL,
+}
+const CHIPOTLE = {
+  name: "Chipotle Mexican Grill",
+  address: "4255 Campus Dr Ste A 116 Irvine, CA 92612",
+  rating: "2.1",
+  reviewCount: 326,
+  categories: "Mexican, Fast Food",
+  image: CHIPOTLE_URL,
+}
+const POPEYES = {
+  name: "Popeyes Louisiana Kitchen",
+  address: "1244 E 17th St Santa Ana, CA 92701",
+  rating: "3.2",
+  reviewCount: 470,
+  categories: "Fast Food, Chicken Wings",
+  image: POPEYES_URL,
+}
+const STARBUCKS = {
+  name: "Starbucks",
+  address: "2134 W Sunset Blvd Los Angeles, CA 90026",
+  rating: "3.2",
+  reviewCount: 217,
+  categories: "Coffee & Tea",
+  image: STARBUCKS_URL,
+}
+
+const CHEESECAKE_FACTORY = {
+  name: "The Cheesecake Factory",
+  address: "189 The Grove Dr, Los Angeles, CA 90036",
+  rating: "3.1",
+  reviewCount: 2059,
+  categories: "Desserts, American",
+  image: CHEESECAKE_FACTORY_URL,
+};
+
+const NOBU = {
+  name: "Nobu",
+  address: "903 N La Cienega Blvd Los Angeles, CA 90069",
+  rating: "3.9",
+  reviewCount: 1719,
+  categories: "Japanese, Sushi Bars, Asian Fusion",
+  image: NOBU_URL,
+}
+
+const GUELAGUETZA = {
+  name: "Guelaguetza Restaurante",
+  address: "3014 W Olympic Blvd Los Angeles, CA 90006",
+  rating: "3.9",
+  reviewCount: 3090,
+  categories: "Mexican",
+  image: GUELAGUETZA_URL,
+}
+
+const IN_N_OUT = {
+name: "In-N-Out Burger",
+address: "7009 Sunset Blvd, Los Angeles, CA 90028",
+rating: "4.5",
+reviewCount: 2127,
+categories: "Burgers, Fast Food",
+image: IN_N_OUT_URL,
+};
+
+const THE_HARD_ROCK_CAFE = {
+name: "Hard Rock Cafe",
+address: "6801 Hollywood Blvd, Los Angeles, CA 90028",
+rating: "3.9",
+reviewCount: 2782,
+categories: "American, Bars, Music Venues",
+image: THE_HARD_ROCK_CAFE_URL,
+};
+
+const BESTIA = {
+name: "Bestia",
+address: "2121 7th Pl, Los Angeles, CA 90021",
+rating: "4.5",
+reviewCount: 2123,
+categories: "Italian, American, Contemporary European",
+image: BESTIA_URL,
+};
+
+const URTH_CAFFE = {
+name: "Urth Caffé",
+address: "8565 Melrose Ave, Los Angeles, CA 90069",
+rating: "4.5",
+reviewCount: 2417,
+categories: "Cafes, Breakfast & Brunch, Organic",
+image: URTH_CAFFE_URL,
+};
+
+const THE_SPOT = {
+name: "The Spot",
+address: "116 S Beverly Dr, Beverly Hills, CA 90212",
+rating: "3.9",
+reviewCount: 1462,
+categories: "Burgers, American, Casual Dining",
+image: THE_SPOT_URL,
+};
+
+const THE_WALKING_STICK = {
+name: "The Walking Stick",
+address: "4111 S Broadway, Los Angeles, CA 90037",
+rating: "4.2",
+reviewCount: 1659,
+categories: "Mexican, Tacos",
+image: THE_WALKING_STICK_URL,
+};
+
+const SHUGA_CAFE = {
+name: "Shuga Cafe",
+address: "3310 1/2 W 6th St, Los Angeles, CA 90020",
+rating: "4.4",
+reviewCount: 2041,
+categories: "Cafe, American, Healthy",
+image: SHUGA_CAFE_URL,
+};
+
+const THE_STONE = {
+name: "The Stone",
+address: "611 S San Pedro St, Los Angeles, CA 90014",
+rating: "4.1",
+reviewCount: 1142,
+categories: "Pizza, Italian",
+image: THE_STONE_URL,
+};
+
+const WINGSTOP = {
+name: "Wingstop",
+address: "5852 Bellflower Blvd, Long Beach, CA 90805",
+rating: "4.0",
+reviewCount: 1427,
+categories: "Chicken Wings, Fast Food",
+image: WINGSTOP_URL,
+};
+
+const PETERS_CAFE = {
+name: "Peter's Cafe",
+address: "1151 S Pacific Coast Hwy, Redondo Beach, CA 90277",
+rating: "3.7",
+reviewCount: 1138,
+categories: "American, Breakfast & Brunch",
+image: PETERS_CAFE_URL,
+};
+
+const THE_BEST_FISH_TACO = {
+name: "The Best Fish Taco",
+address: "1507 E 15th St, Los Angeles, CA 90021",
+rating: "4.3",
+reviewCount: 2487,
+categories: "Tacos, Seafood",
+image: THE_BEST_FISH_TACO_URL,
+};
+
+const THE_RIALTO = {
+name: "The Rialto",
+address: "1670 E 17th St, Santa Ana, CA 92705",
+rating: "4.2",
+reviewCount: 1825,
+categories: "American, Mediterranean",
+image: THE_RIALTO_URL,
+};
+
+const THE_BOWERY = {
+name: "The Bowery",
+address: "1001 S Broadway, Los Angeles, CA 90015",
+rating: "4.0",
+reviewCount: 1893,
+categories: "American, New American",
+image: THE_BOWERY_URL,
+};
+
+const THE_SHED = {
+name: "The Shed",
+address: "2001 S Figueroa St, Los Angeles, CA 90007",
+rating: "4.1",
+reviewCount: 2289,
+categories: "New American",
+image: THE_SHED_URL,
+};
+
+const SUGARFISH = {
+name: "Sugarfish",
+address: "8532 Sunset Blvd, West Hollywood, CA 90069",
+rating: "4.5",
+reviewCount: 1742,
+categories: "Sushi Bars, Japanese",
+image: SUGARFISH_URL,
+};
+
+const LAYLA_CAFE = {
+name: "Layla Cafe",
+address: "2277 E Palmdale Blvd, Palmdale, CA 93550",
+rating: "4.0",
+reviewCount: 951,
+categories: "Cafe, American",
+image: LAYLA_CAFE_URL,
+};
+
+const GRILL_EM_UP = {
+name: "Grill 'Em Up",
+address: "2217 Santa Monica Blvd, Santa Monica, CA 90404",
+rating: "4.1",
+reviewCount: 1123,
+categories: "American, Grill",
+image: GRILL_EM_UP_URL,
+};
+
+const FRANKS_FAMOUS = {
+name: "Frank's Famous",
+address: "1250 E California Blvd, Pasadena, CA 91106",
+rating: "4.0",
+reviewCount: 1435,
+categories: "Burgers, American",
+image: FRANKS_FAMOUS_URL,
+};
+
+const JUST_FIRE = {
+name: "Just Fire",
+address: "1730 W 6th St, Los Angeles, CA 90017",
+rating: "4.0",
+reviewCount: 1378,
+categories: "Burgers, American",
+image: JUST_FIRE_URL,
+};
+
+const KABOB_CHAI = {
+name: "Kabob Chai",
+address: "1430 E 1st St, Los Angeles, CA 90033",
+rating: "4.1",
+reviewCount: 1521,
+categories: "Middle Eastern, Persian",
+image: KABOB_CHAI_URL,
+};
+
+const HAPPY_GARDEN = {
+name: "Happy Garden",
+address: "1512 E Garden Ave, Orange, CA 92867",
+rating: "4.2",
+reviewCount: 1547,
+categories: "Chinese, Seafood",
+image: HAPPY_GARDEN_URL,
+};
+
+const STELLA = {
+name: "Stella",
+address: "2626 4th St, Santa Monica, CA 90405",
+rating: "4.3",
+reviewCount: 1220,
+categories: "Italian",
+image: STELLA_URL,
+};
+
+const THE_KITCHEN = {
+name: "The Kitchen",
+address: "1236 8th St, Santa Monica, CA 90401",
+rating: "4.0",
+reviewCount: 1331,
+categories: "American, New American",
+image: THE_KITCHEN_URL,
+};
+
+const THE_RIDGE = {
+name: "The Ridge",
+address: "2955 10th St, Santa Monica, CA 90405",
+rating: "4.2",
+reviewCount: 1254,
+categories: "New American, American",
+image: THE_RIDGE_URL,
+};
+
+const BOLOVINO = {
+name: "Bolovino",
+address: "1221 S Hill St, Los Angeles, CA 90015",
+rating: "4.3",
+reviewCount: 2156,
+categories: "Italian, European",
+image: BOLOVINO_URL,
+};
+
+const TACOS_AND_TEQUILA = {
+name: "Tacos and Tequila",
+address: "1301 S Beverly Dr, Beverly Hills, CA 90210",
+rating: "3.8",
+reviewCount: 1583,
+categories: "Mexican, Tacos",
+image: TACOS_AND_TEQUILA_URL,
+};
+
+const HORIZON_TAVERN = {
+name: "Horizon Tavern",
+address: "1321 Horizon Ave, Los Angeles, CA 90069",
+rating: "4.1",
+reviewCount: 1765,
+categories: "American, Seafood",
+image: HORIZON_TAVERN_URL,
+};
+
+const GABRIELA = {
+name: "Gabriela",
+address: "1822 S Figueroa St, Los Angeles, CA 90015",
+rating: "4.0",
+reviewCount: 1429,
+categories: "Latin American, Mexican",
+image: GABRIELA_URL,
+};
+
+const AZZURRO = {
+name: "Azzurro",
+address: "3001 S Slauson Ave, Los Angeles, CA 90034",
+rating: "4.2",
+reviewCount: 1983,
+categories: "Italian, American",
+image: AZZURRO_URL,
+};
+
+const THRILLER = {
+name: "Thriller",
+address: "2955 12th St, Los Angeles, CA 90015",
+rating: "4.0",
+reviewCount: 1323,
+categories: "American, Southern",
+image: THRILLER_URL,
+};
+
+const GATEWAY_KITCHEN = {
+name: "Gateway Kitchen",
+address: "5151 S Santa Fe Ave, Los Angeles, CA 90040",
+rating: "4.4",
+reviewCount: 2315,
+categories: "New American, American",
+image: GATEWAY_KITCHEN_URL,
+};
+
+const LA_BROOKLYN = {
+name: "LA Brooklyn",
+address: "1122 6th St, Santa Monica, CA 90405",
+rating: "4.0",
+reviewCount: 1204,
+categories: "Italian",
+image: LA_BROOKLYN_URL,
+};
+
+const CAJUN_QUEEN = {
+name: "Cajun Queen",
+address: "2159 S Figueroa St, Los Angeles, CA 90007",
+rating: "4.5",
+reviewCount: 3194,
+categories: "Cajun, Southern",
+image: CAJUN_QUEEN_URL,
+};
+
+export const RESTAURANTS = [
+  PAPA_JOHNS,
+  BOILING_POINT,
+  MCDONALDS,
+  OOTORO,
+  BURGER_KING,
+  CAVA,
+  CHIPOTLE,
+  POPEYES,
+  STARBUCKS,
+  CHEESECAKE_FACTORY,
+  NOBU,
+  GUELAGUETZA,
+  IN_N_OUT,
+  THE_HARD_ROCK_CAFE,
+  BESTIA,
+  URTH_CAFFE,
+  THE_SPOT,
+  THE_WALKING_STICK,
+  SHUGA_CAFE,
+  THE_STONE,
+  WINGSTOP,
+  PETERS_CAFE,
+  THE_BEST_FISH_TACO,
+  THE_RIALTO,
+  THE_BOWERY,
+  THE_SHED,
+  SUGARFISH,
+  LAYLA_CAFE,
+  GRILL_EM_UP,
+  FRANKS_FAMOUS,
+  JUST_FIRE,
+  KABOB_CHAI,
+  HAPPY_GARDEN,
+  STELLA,
+  THE_KITCHEN,
+  THE_RIDGE,
+  BOLOVINO,
+  TACOS_AND_TEQUILA,
+  HORIZON_TAVERN,
+  GABRIELA,
+  AZZURRO,
+  THRILLER,
+  GATEWAY_KITCHEN,
+  LA_BROOKLYN,
+  CAJUN_QUEEN,
+];
